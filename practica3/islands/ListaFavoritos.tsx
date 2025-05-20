aa091b8a79ea3507f78ad50b06e129b1ae6190ed
Hugo Thibault <123761416+HugoThibaultM@users.noreply.github.com>
2025-05-20 19:30:12 +0200

    ListaFavoritos.tsx

--- practica3/islands/ListaFavoritos.tsx
+++ practica3/islands/ListaFavoritos.tsx
@@ -6,21 +6,16 @@ export default function ListaFavoritos() {
   };
 
   return (
-    <div class="mt-6">
-      <h2 class="text-xl font-bold mb-2">Lista de Favoritos</h2>
+    <div>
+      <h2>Lista de Favoritos</h2>
       {favoritos.value.length === 0 ? (
         <p>No hay películas en la lista.</p>
       ) : (
-        <ul class="space-y-2">
+        <ul>
           {favoritos.value.map((pelicula) => (
-            <li key={pelicula} class="flex justify-between items-center">
+            <li key={pelicula}>
               <span>{pelicula}</span>
-              <button
-                class="text-red-500 hover:text-red-700"
-                onClick={() => eliminar(pelicula)}
-              >
-                Quitar
-              </button>
+              <button onClick={() => eliminar(pelicula)}>Quitar</button>
             </li>
           ))}
         </ul>
